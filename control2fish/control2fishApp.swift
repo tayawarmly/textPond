63e9c84dcdb5bae7d4cdec6136c282d8768c0c66
tayawarmly <avkozlova8@gmail.com>
2023-09-05 18:26:31 +0300

    Initial Commit

---
+++ control2fish/control2fishApp.swift
@@ -0,0 +1,17 @@
+//
+//  control2fishApp.swift
+//  control2fish
+//
+//  Created by Taya on 05.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct control2fishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
