b8d32eb0868e1b9710b2bce10a41948da3a4320d
tayawarmly <avkozlova8@gmail.com>
2023-10-09 19:43:44 +0300

    uniq words and changed SelectedColor logic

---
+++ control2fish/Home/View/InfoView.swift
@@ -0,0 +1,18 @@
+//
+//  InfoView.swift
+//  control2fish
+//
+//  Created by Taya on 06.10.2023.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfoView()
+}
